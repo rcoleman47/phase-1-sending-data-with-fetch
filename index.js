df3b8800e55d7b3ec855449cdc1e1eb19b12e8da
ryanadams <rcoleman.adams@gmail.com>
2022-04-13 22:08:00 -0500

    commit tree a9f3e5439602db37346783cbb8cf28fcd398bf9b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(userName, uEmail){
+  return callFetch(userName, uEmail)
+}
+
+function callFetch(userName,uEmail){
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      'Content-Type': "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: uEmail,
+    })
+  })
+  .then(resp=>resp.json())
+  .then(data=>{
+    const body = document.querySelector('body')
+    const li = document.createElement("li")
+    li.textContent = data.id
+    body.append(li)
+  })
+  .catch((error)=>{
+    const body = document.querySelector('body')
+    const div = document.createElement("li")
+    div.textContent = error
+    body.append(div)
+  })
+}
+
+
+submitData("my name", "my email")
